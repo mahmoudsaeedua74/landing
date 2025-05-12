4437e2042055f22177473d159b3e22d14fd3a585
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-05-12 13:54:27 +0300

    handle banner

--- src/components/footer/Banner.tsx
+++ src/components/footer/Banner.tsx
@@ -10,7 +10,7 @@ export default function Banner() {
   return (
     <div className="bg-second-color z-20 translate-y-12 text-center  m-4 sm:m-0 c text-white py-16 md:py-24 lg:py-40 px-4 md:px-12 lg:px-40 rounded-t-[40px] md:rounded-t-[60px] lg:rounded-t-[100px]">
       <motion.div
-        initial={{ opacity: 0, y: 80 }}
+        initial={{ opacity: 0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: false, amount: 0.3 }}
         transition={{ duration: 0.6, ease: "easeOut" }}
